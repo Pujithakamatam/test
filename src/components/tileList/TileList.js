999f1e90535ea479800f7d71e73a11c3fe4dee29
Kamatam Pujitha <kamatampujitha7@gmail.com>
2024-07-27 08:44:27 +0530

    updated

---
+++ src/components/tileList/TileList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Tile } from '../tile/Tile';
+
+// This components receive from appointments and contacts 
+export const TileList = () => {
+  
+  return (
+    <div>
+      
+    </div>
+  );
+};
